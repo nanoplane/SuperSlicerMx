b83b3e591c8680bb9513f23817c4237496fbf16f
Marcel Rivard <marcel@rivard.org>
2021-03-08 13:37:24 -0800

    ongoing saga of adding mixing extruder support

--- src/libslic3r/Config.hpp
+++ src/libslic3r/Config.hpp
@@ -194,6 +194,8 @@ enum ConfigOptionType : uint16_t{
     coBools         = coBool + coVectorType,
     // a generic enum
     coEnum          = 9,
+    // vector of enum
+    coEnums         = coEnum + coVectorType
 };
 
 enum ConfigOptionMode {
