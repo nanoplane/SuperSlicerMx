763e30245ad566b37120fd92c0316e22b4a42092
Marcel Rivard <marcel@rivard.org>
2021-03-19 19:20:10 -0700

    for some reason MAX() exists on the Mac but not on Windows....
    replaced it with std::max()

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -1228,7 +1228,7 @@ ExtruderMixAndChangePts::ExtruderMixAndChangePts(int num_filaments, std::string 
             double dh =mheights[0];
             for (int i = 0; i < mhsize -1; i++) {
                 dh = mheights[i];
-                mheights[i] = MAX(this_height, min_height);
+                mheights[i] = std::max(this_height, min_height);
                 this_height -= dh;
             }
         } else {
@@ -1237,7 +1237,7 @@ ExtruderMixAndChangePts::ExtruderMixAndChangePts(int num_filaments, std::string 
             double dh =mheights[0];
             for (int i = 0; i < mheights.size(); i++) {
                 dh = mheights[i];
-                mheights[i] = MAX(this_height - dh, min_height);
+                mheights[i] = std::max(this_height - dh, min_height);
                 this_height -= dh;
             }
 
