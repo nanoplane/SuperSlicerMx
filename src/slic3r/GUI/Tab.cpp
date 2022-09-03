9f468e6883ebe9720cb444a07fb0b80aa6f4b239
Marcel <marcel@rivard.org>
2022-09-03 15:13:49 -0700

    removed old logo file updated Tab to properly manage adding and removing extruders (bug in latest version)

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -3146,6 +3146,10 @@ void TabPrinter::build_unregular_pages(bool from_initial_build/* = false*/)
             }
         m_has_single_extruder_MM_page = false;
     }
+    if (m_has_single_extruder_MM_page) {
+        n_before_extruders++;
+    }
+    
     if (from_initial_build ||
         (m_extruders_count > 1 && m_config->opt_bool("single_extruder_multi_material") && !m_has_single_extruder_MM_page)) {
         // create a page, but pretend it's an extruder page, so we can add it to m_pages ourselves
